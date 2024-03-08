e358d63bf6fb26a22f4941fe11a6cade4ff532cd
Vamshidhar <reddyvamshi607@gmail.com>
2024-03-08 17:20:12 +0530

    update the location of the backend.sh

--- backend.sh
+++ backend.sh
@@ -3,6 +3,8 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 mkdir /app
+cp backend.service /etc/systemd/system/backend.service
+
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
@@ -10,7 +12,6 @@ unzip /tmp/backend.zip
 cd /app
 npm install
 
-cp backend.service /etc/systemd/system/backend.service
 
 systemctl daemon-reload
 systemctl enable backend
