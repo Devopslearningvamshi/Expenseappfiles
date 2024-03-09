f85ee6d341a6d2a14846ecda9197b7a85855be92
Vamshidhar <reddyvamshi607@gmail.com>
2024-03-09 07:56:26 +0530

    All new files are add

---
+++ backend.sh
@@ -0,0 +1,28 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd expense
+
+mkdir /app
+
+rm -rf /app
+cd /app
+
+cp backend.service /etc/systemd/system/backend.service
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+
+cd /app
+unzip /tmp/backend.zip
+
+npm install
+systemctl daemon-reload
+
+systemctl enable backend
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h 172.31.89.121 -uroot -pExpenseApp@1 < /app/schema/backend.sql
