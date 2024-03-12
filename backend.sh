142d4896266b35c9f152e9cb2373dee0961822b7
PATEL VAMSHI DHAR REDDY <49507145+patelvamshi@users.noreply.github.com>
2024-03-12 14:47:57 +0530

    Update backend.sh
    Backend script is udpated

--- backend.sh
+++ backend.sh
@@ -3,29 +3,23 @@ dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
 
-
 useradd expense
-cp backend.service /etc/systemd/system/backend.service
 
-rm -rf /app
-mkdir /app
+mkdir /app 
 
-
-cd /app
-
-
-
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-
-cd /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
+cd /app 
 unzip /tmp/backend.zip
 
-npm install
+cd /app 
+npm install 
+
 systemctl daemon-reload
 
-systemctl enable backend
+systemctl enable backend 
 systemctl start backend
 
-dnf install mysql -y
+dnf install mysql -y 
 
-mysql -h 172.31.84.193 -uroot - ExpenseApp@1 < /app/schema/backend.sql
+
+mysql -h 172.31.81.81 -uroot -pExpenseApp@1 < /app/schema/backend.sql 
