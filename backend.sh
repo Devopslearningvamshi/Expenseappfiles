11ab7264309f8c874f5585a7da1387612a0ae8cb
PATEL VAMSHI DHAR REDDY <49507145+patelvamshi@users.noreply.github.com>
2024-03-12 15:09:49 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -7,6 +7,7 @@ cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 
+rm-rf /app
 mkdir /app 
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
